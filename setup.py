169338bb57117520f7865c21e2bd0a78eb880de8
Anush Subba <anushsubba101@gmail.com>
2026-07-20 12:07:52 +0545

    requirements changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="1.0",
+    author="Anush Subba",
+    author_email="anushsubba101@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
